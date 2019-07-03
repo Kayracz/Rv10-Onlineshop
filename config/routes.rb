331d5eb84e4a6397b6e028c97d2e48349143c9b6
Kayra Czerniewicz <kayracz@gmail.com>
2019-07-03 19:52:25 -0400

    more

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-
   root 'pages#index'
   get 'shop' => 'pages#shop'
+  get 'about' => 'pages#about'
 
   resources :carts
   resources :product_items
