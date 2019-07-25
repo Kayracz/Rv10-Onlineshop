2c8f1f941fb5b66d39246534fabe246506fe3c21
amiralles <amiralles.net@gmail.com>
2019-07-25 11:22:39 -0300

    format order.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,20 @@
 class OrdersController < ApplicationController
-
   include CurrentCart
   before_action :set_cart, only: [:new, :create, :show, :contraentrega]
   before_action :set_order, only: [:show, :edit, :destroy, :contraentrega]
 
-
-def index
-  @orders = Order.all
-end
-
-def new
-  if @cart.product_items.empty?
-    redirect_to product_url, notice: 'Your Cart is Empty'
-    return
+  def index
+    @orders = Order.all
   end
-  @order = Order.new
-  @client_token = Braintree::ClientToken.generate
 
-end
+  def new
+    if @cart.product_items.empty?
+      redirect_to product_url, notice: 'Your Cart is Empty'
+      return
+    end
+    @order = Order.new
+    @client_token = Braintree::ClientToken.generate
+  end
 
   def create
     @order = Order.new(order_params)
@@ -40,39 +37,38 @@ end
     end
   end
 
-    def contraentrega
+  def contraentrega
     @order = Order.new(order_params)
     if @order.save
-        @order.add_product_items_from_cart(@cart)
-        Cart.destroy(session[:cart_id])
-        session[:cart_id] = nil
-        redirect_to contraentrega_path, notice: 'Thank You for Your Order!'
-      else
-        flash[:error] = 'Check Your Cart'
-        redirect_to root_url, alert: @result.message
-        @order.destroy
-      end
+      @order.add_product_items_from_cart(@cart)
+      Cart.destroy(session[:cart_id])
+      session[:cart_id] = nil
+      redirect_to contraentrega_path, notice: 'Thank You for Your Order!'
     else
-      render :new
+      flash[:error] = 'Check Your Cart'
+      redirect_to root_url, alert: @result.message
+      @order.destroy
     end
+  else
+    render :new
+  end
 
+  def show
+  end
 
-def show
-end
+  def destroy
+    @order.destroy
+    redirect_to root_url, notice: 'Order deleted'
+  end
 
-def destroy
- @order.destroy
- redirect_to root_url, notice: 'Order deleted'
-end
-
-private
-
+  private
   def set_order
     @order = Order.find(params[:id])
   end
 
   def order_params
-    params.require(:order).permit(:name, :email, :phone, :address, :city, :country, :Nit, :Notas)
+    params.require(:order).permit(:name, :email, :phone, :address, 
+                                  :city, :country, :Nit, :Notas)
   end
 
   def charge
@@ -80,6 +76,4 @@ private
       amount: @cart.total_price,
       payment_method_nonce: 'fake-valid-nonce')
   end
-
-
 end
