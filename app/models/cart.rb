e56930d9440bc6634b38a856c4d31ab9564ff610
amiralles <amiralles.net@gmail.com>
2019-07-25 17:55:39 -0300

    add same product in different sizes.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,13 @@ class Cart < ApplicationRecord
   has_many :product_items, dependent: :destroy
 
   def add_product(product_id, size_id)
-    current_item = product_items.find_by(product_id: product_id)
+    # The way to uniquely identify the product is by
+    # its id *and* its size.
+    # This allows users to add the same product in
+    # different sizes to the cart.
+    current_item = product_items.where(
+      product_id: product_id,
+      size_id: size_id).first
 
     if current_item
       current_item.quantity += 1
