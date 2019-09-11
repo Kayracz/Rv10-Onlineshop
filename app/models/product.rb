f3168d9add2374081b0dfabcc711d05fa9a1b61d
amiralles <amiralles.net@gmail.com>
2019-09-11 10:45:14 -0300

    filter and search.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  include PgSearch
+
   mount_uploader :photo, PhotoUploader
   before_destroy :ensure_not_product_item
 
@@ -12,16 +14,55 @@ class Product < ApplicationRecord
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
 
-  def self.search(search)
-   if search
-     where(["codigo LIKE ?","%#{search}%"])
-   else
-    all
+  pg_search_scope :full_text_search, 
+    against: %i(title description size category subcategory color),
+    :using => {
+      :tsearch => {
+        :dictionary => "spanish",
+        :prefix => true,
+        :highlight => { }
+      }
+    }
+
+  def self.filter(color, price_range, size)
+    sql_filter = prepare_sql_filter(color, price_range, size)
+    if sql_filter.empty?
+      all
+    else
+      where(sql_filter)
+    end
   end
-end
+
+  def self.color
+    @@color_options ||= select('DISTINCT color').map(&:color).sort!
+  end
+
+  def self.price_range
+    @@price_range_options ||= 
+      price_ranges.collect {|range| ["#{range.first} - #{range.last}", range] }
+  end
+
+  def self.size
+    @@size_options ||= Size.all.order(:id).collect {|size| [size.name, size.id]}
+  end
+
+  def self.price_ranges
+    # TODO: Use sensible values.
+    @@price_ranges ||= 
+      [[1, 500], [501, 1000], [1001, 2000], [2001, 5000], [5000, 10000]]
+  end
+
+
+  def self.search(search)
+    if search
+      where(["codigo LIKE ?","%#{search}%"])
+    else
+      all
+    end
+  end
 
   def total_price
-   product.price * quantity
+    product.price * quantity
   end
 
   def ensure_not_product_item
@@ -33,21 +74,42 @@ end
     end
   end
 
- def self.women
-   where(subcategory: 'women')
- end
+  def self.women
+    where(subcategory: 'women')
+  end
 
- def self.men
-   where(subcategory: 'men')
- end
+  def self.men
+    where(subcategory: 'men')
+  end
 
   def self.kids
-   where(subcategory: 'kids')
- end
+    where(subcategory: 'kids')
+  end
 
- def sale_price
-  price * promo.to_i * 10 / 100
-end
+  def sale_price
+    price * promo.to_i * 10 / 100
+  end
 
+  private
+
+  def self.prepare_sql_filter(color, price_range, size)
+    # Colors
+    # Price range
+    # Sizes (products that are available in stock in a precise size)
+    filters = []
+    if color.present?
+      filters << "lower(color) = '#{color.downcase}'"
+    end
+    
+    if price_range.present?
+      filters << "price BETWEEN #{price_range.first} AND #{price_range.last}"
+    end
+
+    if size.present?
+      filters << "lower(size) = '#{size.downcase}'"
+    end
+
+    filters.join(" AND ")
+  end
 end
 
